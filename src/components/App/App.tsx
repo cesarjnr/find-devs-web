9e5f86758b8c29e3720abbc7fceccf48ea44f601
César <cesar.jnr@hotmail.com>
2020-02-04 22:05:55 -0300

    Cria a caixa que contém o formulário de cadastro

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,36 @@ import React from 'react';
 import '../../global.css';
 
 const App = () => (
-  <div />
+  <div id="app">
+    <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <label htmlFor="github_username">
+          Usuário do Github
+          <input name="github_username" id="github_username" required />
+        </label>
+
+        <label htmlFor="techs">
+          Tecnologias
+          <input name="techs" id="techs" required />
+        </label>
+
+        <div className="input-group">
+          <label htmlFor="latitude">
+            Latitude
+            <input name="latitude" id="latitude" required />
+          </label>
+
+          <label htmlFor="longitude">
+            Latitude
+            <input name="longitude" id="longitude" required />
+          </label>
+        </div>
+
+        <button type="submit">Salvar</button>
+      </form>
+    </aside>
+  </div>
 );
 
 export default App;
