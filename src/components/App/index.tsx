d395501560ec063e04fbd50d67a29e7fb9877d42
César <cesar.jnr@hotmail.com>
2020-02-11 20:40:42 -0300

    Cria o diretório pages e a página Home

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,43 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
-import '../../global.css';
-import './index.css';
-import DevForm from './DevForm';
-import DevItem from './DevItem';
-import { DevsTypes, Dev, DevFormData } from '../../store/ducks/devs/types';
-import { ApplicationState } from '../../store/index';
+import Home from '../../pages/Home';
 
-const App = () => {
-  const dispatch = useDispatch();
-  const devs = useSelector((state: ApplicationState) => state.devs.data);
-
-  useEffect(() => {
-    dispatch({ type: DevsTypes.FETCH_REQUEST });
-  }, [dispatch]);
-
-  const addDev = async (formData: DevFormData) => {
-    // const response = await api.post('/devs', formData);
-
-    // setDevs([...devs, response.data]);
-  };
-
-  return (
-    <div id="app">
-      <aside>
-        <strong>Cadastrar</strong>
-        <DevForm parentHandleSubmit={addDev} />
-      </aside>
-
-      <main>
-        <ul>
-          {devs.map(dev => (
-            <DevItem key={dev._id} dev={dev} />
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
-};
+const App = () => <Home />;
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import '../../global.css';
+import './styles.css';
+import DevForm from './DevForm';
+import DevItem from './DevItem';
+import { DevsTypes, Dev, DevFormData } from '../../store/ducks/devs/types';
+import { ApplicationState } from '../../store/index';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const devs = useSelector((state: ApplicationState) => state.devs.data);
+
+  useEffect(() => {
+    dispatch({ type: DevsTypes.FETCH_REQUEST });
+  }, [dispatch]);
+
+  const addDev = async (formData: DevFormData) => {
+    // const response = await api.post('/devs', formData);
+
+    // setDevs([...devs, response.data]);
+  };
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm parentHandleSubmit={addDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
